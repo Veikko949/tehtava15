ebdc7d6485bb0e2f75a6e0953e8a4ec40788a112
Veikko Pihlajamaa <veikko.pihlajamaa@edu.keuda.fi>
2021-09-06 12:40:30 +0300

    Add project files.

---
+++ tehtava15/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace tehtava15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.aika_label1 = new System.Windows.Forms.Label();
+            this.kaynista_button1 = new System.Windows.Forms.Button();
+            this.pysayta_button1 = new System.Windows.Forms.Button();
+            this.uudeleen_button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // aika_label1
+            // 
+            this.aika_label1.AutoSize = true;
+            this.aika_label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aika_label1.Location = new System.Drawing.Point(40, 22);
+            this.aika_label1.Name = "aika_label1";
+            this.aika_label1.Size = new System.Drawing.Size(603, 108);
+            this.aika_label1.TabIndex = 0;
+            this.aika_label1.Text = "00:00:00.000";
+            // 
+            // kaynista_button1
+            // 
+            this.kaynista_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kaynista_button1.Location = new System.Drawing.Point(58, 134);
+            this.kaynista_button1.Name = "kaynista_button1";
+            this.kaynista_button1.Size = new System.Drawing.Size(148, 45);
+            this.kaynista_button1.TabIndex = 1;
+            this.kaynista_button1.Text = "Start";
+            this.kaynista_button1.UseVisualStyleBackColor = true;
+            this.kaynista_button1.Click += new System.EventHandler(this.kaynista_button1_Click);
+            // 
+            // pysayta_button1
+            // 
+            this.pysayta_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pysayta_button1.Location = new System.Drawing.Point(266, 133);
+            this.pysayta_button1.Name = "pysayta_button1";
+            this.pysayta_button1.Size = new System.Drawing.Size(141, 45);
+            this.pysayta_button1.TabIndex = 2;
+            this.pysayta_button1.Text = "Stop";
+            this.pysayta_button1.UseVisualStyleBackColor = true;
+            this.pysayta_button1.Click += new System.EventHandler(this.pysayta_button1_Click);
+            // 
+            // uudeleen_button1
+            // 
+            this.uudeleen_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uudeleen_button1.Location = new System.Drawing.Point(463, 133);
+            this.uudeleen_button1.Name = "uudeleen_button1";
+            this.uudeleen_button1.Size = new System.Drawing.Size(147, 45);
+            this.uudeleen_button1.TabIndex = 3;
+            this.uudeleen_button1.Text = "Reset";
+            this.uudeleen_button1.UseVisualStyleBackColor = true;
+            this.uudeleen_button1.Click += new System.EventHandler(this.uudeleen_button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 225);
+            this.Controls.Add(this.uudeleen_button1);
+            this.Controls.Add(this.pysayta_button1);
+            this.Controls.Add(this.kaynista_button1);
+            this.Controls.Add(this.aika_label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label aika_label1;
+        private System.Windows.Forms.Button kaynista_button1;
+        private System.Windows.Forms.Button pysayta_button1;
+        private System.Windows.Forms.Button uudeleen_button1;
+    }
+}
+
---
+++ tehtava15/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace tehtava15
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch ajastin;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            aika_label1.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", ajastin.Elapsed);
+        }
+
+        private void kaynista_button1_Click(object sender, EventArgs e)
+        {
+            ajastin.Start();
+        }
+
+        private void pysayta_button1_Click(object sender, EventArgs e)
+        {
+            ajastin.Stop();
+        }
+
+        private void uudeleen_button1_Click(object sender, EventArgs e)
+        {
+            ajastin.Reset();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ajastin = new Stopwatch();
+        }
+    }
+}
